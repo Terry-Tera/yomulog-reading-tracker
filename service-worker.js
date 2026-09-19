8dfaa1cef2ca7a040cacdbdc5ad003c4f2c9078c
Terry-Tera <takeshi.terasaki3110@gmail.com>
2026-09-19 20:48:18 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,9 @@
-const cacheName = 'yomulog-v1';
+const cacheName = 'yomulog-v2';
 const files = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(files))));
+self.addEventListener('activate', (event) => event.waitUntil(
+  caches.keys().then((keys) => Promise.all(
+    keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+  )).then(() => self.clients.claim())
+));
 self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
